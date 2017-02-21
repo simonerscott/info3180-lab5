6bf2356300791fc7e468b0257658dd7986cd0123
simonerscott <rhoniscott@yahoo.com>
2017-02-21 15:02:52 +0000

    Step 5 of Lab 5 Completed

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,8 @@ from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "change this to be a more random key"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://user:password@localhost/database"
+app.config['SECRET_KEY'] = "supersecretpassword"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://lab5:lab5password@localhost/lab5"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 
 db = SQLAlchemy(app)
@@ -12,6 +12,8 @@ db = SQLAlchemy(app)
 # Flask-Login login manager
 login_manager = LoginManager()
 login_manager.init_app(app)
+#login_manager.login_view = 'login'
+
 
 app.config.from_object(__name__)
 from app import views
