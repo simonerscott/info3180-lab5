331b99e0937d41f5aadf5261fc51d1b85ddc3125
simonerscott <rhoniscott@yahoo.com>
2017-02-22 15:36:22 +0000

    Completed Step 8 of Lab 5

--- app/views.py
+++ app/views.py
@@ -25,27 +25,40 @@ def home():
 def about():
     """Render the website's about page."""
     return render_template('about.html')
+    
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
-    if request.method == "POST":
+    if request.method == "POST" and form.validate_on_submit():
         # change this to actually validate the entire form submission
         # and not just one field
-        if form.username.data:
+        # if form.username.data:
             # Get the username and password values from the form.
+            username = form.username.data
+            password = form.password.data
+            print username,password
 
             # using your model, query database for a user based on the username
             # and password submitted
             # store the result of that query to a `user` variable so it can be
             # passed to the login_user() method.
-
+            user = UserProfile.query.filter_by(username = username, password = password).first()
+            
+            
+            if user is not None:
             # get user id, load into session
-            login_user(user)
+                login_user(user)
+                flash('Logged in successfully.', 'success')
+                next = request.args.get('next')
+                return redirect(url_for('secure_page'))
+            else:
+                flash('Username or Password is incorrect.', 'danger')
 
             # remember to flash a message to the user
-            return redirect(url_for("home")) # they should be redirected to a secure-page route instead
-    return render_template("login.html", form=form)
+            # return redirect(url_for("home")) # they should be redirected to a secure-page route instead
+    return render_template("login.html", form = form)
+    
 
 # user_loader callback. This callback is used to reload the user object from
 # the user ID stored in the session
