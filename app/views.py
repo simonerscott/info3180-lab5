2752b2d8acda9789a209fa470431608ba2cbf1e8
simonerscott <rhoniscott@yahoo.com>
2017-02-22 16:25:34 +0000

    Completed Step 10 of Lab 5

--- app/views.py
+++ app/views.py
@@ -69,7 +69,13 @@ def login():
     return render_template("login.html", form = form)
 
 
-
+@app.route("/logout")    
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out.', 'danger')
+    return redirect(url_for('home'))
+    
 
 # user_loader callback. This callback is used to reload the user object from
 # the user ID stored in the session
